e7780734d408cced5546ea545b7e4bdbf55b7c67
Amee Srivastava <52029498+amee6394@users.noreply.github.com>
2020-12-04 14:43:07 +0530

    Add files via upload

--- typing.js
+++ typing.js
@@ -4,10 +4,16 @@ const fixedtext = document.querySelector("#origin-text p").innerHTML;
 const  resetbtn= document.querySelector("#reset");
 const timeDisplay= document.querySelector(".timer");
 var time=[0,0,0,0];
-console.log(wordInput);
 var int;
 var stop=false;
-
+var error=0;
+var wpm;
+var ctime;
+var text;
+var text1;
+var time1="";
+var textlenght=0;
+var words;
 
 
 function start(){
@@ -19,25 +25,34 @@ if(textlenght===0 && !stop){
 }
 function spell(){
     var text=currentWord.value;
+    console.log(text.split(" "));
+    var words=text.split(" ").length;
+    var wpm=parseInt((words/time1)*60);
     var text1=fixedtext.substring(0,text.length);
     if(text == fixedtext){
         clearInterval(int);
-       wordInput.style.borderColor="#429898";
-       console.log("h");
+       wordInput.style.borderColor="green";
+       document.getElementById("demo3").innerHTML="Words per minute="+wpm;
     }
     else{
         if(text == text1){
-            wordInput.style.borderColor="green";
-            console.log("h3");
+            wordInput.style.borderColor="#429898";
         }
         else{
             wordInput.style.borderColor="red";
-            console.log("h2");
+            error++;
         }
     }
    
 }
 function run(){
+    var acc=(((fixedtext.length-error)/fixedtext.length)*100);
+    document.getElementById("demo1").innerHTML="accuracy="+acc+"%";
+    if(acc<=0){
+        document.getElementById("demo1").innerHTML="accuracy="+0+"%";
+    }
+    document.getElementById("demo2").innerHTML="Error="+error;
+    time1=time[0]*60+time[1];
     var ctime=time[0]+":"+time[1]+":"+time[2];
     timeDisplay.innerHTML=ctime;
     time[3]++;
@@ -54,6 +69,12 @@ function reset(){
     int=null;
     currentWord.value="";
     timeDisplay.innerHTML="00:00:00";
+    document.getElementById("demo1").innerHTML="Correct text typed Accuracy";
+    document.getElementById("demo2").innerHTML="Errors";
+    document.getElementById("demo2").innerHTML="Words per minute";
+    count=0;
+    error=0;
+
 }
 currentWord.addEventListener("keypress",start);
 currentWord.addEventListener("keyup",spell);
